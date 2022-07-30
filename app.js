cfaa7790e1a743d35a1d4f2f133099b4f25896d7
dianalleon <diana.kotoko115@gmail.com>
2022-07-30 12:07:11 -0500

    cambios

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 //Registrar Materia
+
 const materias = JSON.parse(localStorage.getItem('materia')) ?? [];
+const canceladas = JSON.parse(localStorage.getItem('cancelada')) ?? [];
 
-let formulario = document.getElementById('formulario');
+mostrarTabla();
+let tabla = document.querySelector("#table");
 
+//Guardar en el formulario
+var formulario = document.getElementById('formulario');
 formulario.addEventListener('submit', function (e) {
     e.preventDefault();
 
@@ -15,12 +20,75 @@ formulario.addEventListener('submit', function (e) {
     }
 
     materias.push(materia);
-    console.log("Materia Registrada");
-    localStorage.setItem('materia', JSON.stringify(materias));
     definitivaFinal();
-    
+    cambiarColor();
+    mostrarTabla();   
+    localStorage.setItem('materia', JSON.stringify(materias));
 });
 
-function mostrarMaterias() {
-    return materias;
+
+//Calcular Definitiva
+function definitivaFinal(){
+    materias.map(item => {
+  
+        let def = (((Number(item.pp) + Number(item.sp) + Number(item.tp))/3)*0.7+(item.ex* 0.3))
+    
+        if(def>=2.95&&def<3){
+    
+          def=3
+        }
+          item.def= def
+    
+      })
 }
+
+//Cambiar el Color segun la defitiva
+function cambiarColor(){
+    materias.map( item =>{
+    
+            if(item.def >0.0 && item.def<=2.9){
+                item.color="red"
+            }else if(item.def >=3.0 && item.def<=3.9){
+                item.color="yellow"
+            }else if(item.def >=4.0 && item.def<=5.0){
+                item.color="green"
+            }
+        })
+}
+
+function mostrarTabla() {
+    let result="";
+    materias.forEach((item) => {
+      result += `
+        <tr>
+            <td>${item.nombre}</td>
+            <td>${item.pp}</td>
+            <td>${item.sp}</td>
+            <td>${item.tp}</td>
+            <td>${item.ex}</td>
+            <td>${item.def}</td>
+            <td>
+                <button id="button" type="submit">Cancelar</button>
+                <button id="button" type="submit">Editar</button>
+            </td>
+      </tr>`
+    })
+      let tab = document.querySelector("#tab");
+      tab.innerHTML = result;
+  }
+
+    var canc = document.getElementById('button');
+    canc.addEventListener('click', materiaCancelada);
+
+    function materiaCancelada(){
+        let cancelada = materias.find(item => item.nombre == nombre);
+        canceladas.push(cancelada);
+        localStorage.removeItem(cancelada);
+        mostrarTabla();
+        mostrarCancelada();
+    }
+
+    function mostrarCancelada(){
+        return canceladas;
+    }
+
